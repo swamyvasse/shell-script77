f07854b69e401e705abc970e007a94e6f4845eee
swamy vasse <swamyvasse@gmail.com>
2024-03-26 11:26:36 +0530

    varibles added

--- helloworld.sh
+++ helloworld.sh
@@ -3,11 +3,11 @@ echo "that is she bang it should inform to linux server
 how to compile or excute the script
 it is the first line of sheel script"
 
-varible1=&sample
-varible2=&demo
+PERSON1=ramesh
+PERSON2=suresh
 
-echo "varible1 : hi how are you varible2?"
-echo "varible2 : i am fine what about you varible1?"
-echo "varible1 : good what is happening varible2?"
-echo "varible2 : currently i am learning aws devops"
+echo "$PERSON1 : hi how are you $PERSON2?"
+echo "$PERSON2 : i am fine what about you $PERSON1?"
+echo "$PERSON1 : good what is happening $PERSON2?"
+echo "$PERSON2 : currently i am learning aws devops"
 

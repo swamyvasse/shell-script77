0d2cabdffbc974fe35d034efa472c111fa079c39
swamy vasse <swamyvasse@gmail.com>
2024-03-26 11:56:38 +0530

    add the two numbers

---
+++ 04-varible.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#this is the comment develop program
+
+NUMBER1=$1
+NUMBER2=$2
+#i need to run command inside the shell and stores the output in varible
+
+SUM=$((NUMBER1+NUMBER2))
+
+ echo "the sum of value in two numbers are : "

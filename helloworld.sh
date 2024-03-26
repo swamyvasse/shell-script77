04cd1e1106858e124215e5b55231066eee6cc593
swamy vasse <swamyvasse@gmail.com>
2024-03-26 11:06:51 +0530

    varibles

--- helloworld.sh
+++ helloworld.sh
@@ -3,8 +3,11 @@ echo "that is she bang it should inform to linux server
 how to compile or excute the script
 it is the first line of sheel script"
 
-echo "ramesh : hi how are you suresh?"
-echo "suresh : i am fine what about you ramesh?"
-echo "ramesh : good what is happening suresh?"
-echo "suresh : currently i am learning aws devops"
+varible1=&sample
+varible2=&demo
+
+echo "varible1 : hi how are you varible2?"
+echo "varible2 : i am fine what about you varible1?"
+echo "varible1 : good what is happening varible2?"
+echo "varible2 : currently i am learning aws devops"
 

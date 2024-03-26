ce360f6ebc07a8fb4fc5e51727b9e6b916c014f8
swamy vasse <swamyvasse@gmail.com>
2024-03-26 12:36:45 +0530

    commitid changes

--- 06-varible.sh
+++ 06-varible.sh
@@ -4,4 +4,10 @@ PERSON1=$1
 PERSON2=$2
 
 echo "$PERSON1: Hi how are you?"
-echo "$PERSON2: iam fine about you PERSON1"
+echo "$PERSON2: iam fine about you $PERSON1"
+
+echo "please enter you name"
+
+raed USERNAME
+
+echo "useranme enterd :$USERNAME"

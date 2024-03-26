f94ff167c26a671fb34592b219ee15bc96d93843
swamy vasse <swamyvasse@gmail.com>
2024-03-26 11:02:51 +0530

    conversation between two people

--- helloworld.sh
+++ helloworld.sh
@@ -3,4 +3,8 @@ echo "that is she bang it should inform to linux server
 how to compile or excute the script
 it is the first line of sheel script"
 
-echo "this is hello world basic shell script file"
+echo "ramesh : hi how are you suresh?"
+echo "suresh : i am fine what about you ramesh?"
+echo "ramesh : good what is happening suresh?"
+echo "suresh : currently i am learning aws devops"
+

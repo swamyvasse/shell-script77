711b0f520896f981998ce11357ddba3280ddf23e
swamy vasse <swamyvasse@gmail.com>
2024-03-26 11:44:29 +0530

    varible file added

---
+++ 03-varible.sh
@@ -0,0 +1,12 @@
+#/bin/bash 
+echo "that is she bang it should inform to linux server
+how to compile or excute the script
+it is the first line of sheel script"
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 : hi how are you $PERSON2?"
+echo "$PERSON2 : i am fine what about you $PERSON1?"
+echo "$PERSON1 : good what is happening $PERSON2?"
+echo "$PERSON2 : currently i am learning aws devops"

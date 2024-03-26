0892080ebbea51ce3d4b5770b91c39f944404953
swamy vasse <swamyvasse@gmail.com>
2024-03-26 09:44:36 +0530

    some changes

--- helloworld.sh
+++ helloworld.sh
@@ -1,6 +1,6 @@
 #/bin/bash 
-that is she bang it should inform to linux server 
+echo "that is she bang it should inform to linux server
 how to compile or excute the script
-it is the first line of sheel script
+it is the first line of sheel script"
 
 echo "this is hello world basic shell script file"
